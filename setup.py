5459b720b20099e236738b2f05d9236364f3773c
Johan Hake <hake.dev@gmail.com>
2012-05-07 12:30:41 +0200

    Added gotran script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "Johan Hake (hake.dev@gmail.com)"
 __copyright__ = "Copyright (C) 2010 " + __author__
-__date__ = "2012-02-22 -- 2012-02-23"
+__date__ = "2012-02-22 -- 2012-05-07"
 __license__  = "GNU LGPL Version 3.0 or later"
 
 
@@ -10,20 +10,36 @@ __license__  = "GNU LGPL Version 3.0 or later"
 from distutils.core import setup
 from os.path import join as pjoin
 import glob
+import platform
+import sys
 
 # Version number
 major = 2
 minor = 0
 
+scripts = [pjoin("scripts", "gotran2")]
+
+if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
+    # In the Windows command prompt we can't execute Python scripts
+    # without a .py extension. A solution is to create batch files
+    # that runs the different scripts.
+    batch_files = []
+    for script in scripts:
+        batch_file = script + ".bat"
+        f = open(batch_file, "w")
+        f.write('python "%%~dp0\%s" %%*\n' % split(script)[1])
+        f.close()
+        batch_files.append(batch_file)
+    scripts.extend(batch_files)
+
 setup(name = "Gotran2",
-      version = "%d.%d" % (major, minor),
+      version = "{}.{}".format(major, minor),
       description = """
       A declarative language describing ordinary differential equations.
       """,
       author = __author__.split("(")[0],
       author_email = __author__.split("(")[1][:-1],
       packages = ["gotran2", "gotran2.common", "gotran2.models"],
-      package_dir = {"gotran2": "src",
-                     "gotran2.common":pjoin("src","common"),
-                     "gotran2.common":pjoin("src","models"),
-                     })
+      package_dir = {"gotran2": "src"},
+      scripts = scripts,
+      )
