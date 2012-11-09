1ac14ad3d093ee7671a2f06813c9ff7da44a6c77
Johan Hake <hake.dev@gmail.com>
2012-11-09 10:32:45 +0100

    Fixes for varargout

--- gotran2/codegeneration/codegenerator.py
+++ gotran2/codegeneration/codegenerator.py
@@ -766,8 +766,8 @@ class MatlabCodeGenerator(CodeGenerator):
             init_values.append("x0({0}) = {1}".format(ind + 1, state.init))
             state_names.append("state_names{{{0}}} = \"{1}\"".format(ind + 1, state.name))
 
-        init_values.append("varargout{1} = x0")
-        state_names.append("varargout{2} = state_names")
+        init_values.append("varargout(1) = {x0}")
+        state_names.append("varargout(2) = {state_names}")
 
         # Add bodys to code
         body_lines.append("if nargout == 2")
@@ -799,7 +799,7 @@ class MatlabCodeGenerator(CodeGenerator):
 
         body_lines = [""]
         
-        body_lines.append("if nargin != 2 || nargin != 3")
+        body_lines.append("if !(nargin == 2 || nargin == 3)")
         body_lines.append(["error('Expected 2-3 input arguments.')"])
         body_lines.append("")
         body_lines.append("if nargin == 2")
