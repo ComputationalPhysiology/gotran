777a88c15ecfb56c4ae792bf015922e0e49010ea
Henrik Finsberg <henriknf@simula.no>
2022-06-21 21:54:32 +0200

    Bump version: 2022.2.0 → 2022.2.1

--- gotran/__init__.py
+++ gotran/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Gotran. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2022.2.0"
+__version__ = "2022.2.1"
 
 
 # import algorithms
